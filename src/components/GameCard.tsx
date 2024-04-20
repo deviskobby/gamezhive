8cd97c8b609928b225a0b6072cc65d6f7cef3269
Elabscode <elabscodesocial@gmail.com>
2024-04-20 20:50:36 +0000

    Display fetched games in GameCard component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,10 +1,18 @@
-import { Card, CardBody, Heading } from "@chakra-ui/react";
-
-const GameCard = () => {
+import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import Game from "../entities/Game";
+import getCroppedImageUrl from "../services/image-url";
+import { Link } from "react-router-dom";
+interface Props {
+  game: Game;
+}
+const GameCard = ({ game }: Props) => {
   return (
     <Card>
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">Game Name</Heading>
+        <Heading fontSize="2xl">
+          <Link to={`/games/${game.slug}`}>{game.name}</Link>
+        </Heading>
       </CardBody>
     </Card>
   );
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,38 +1,28 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 import GameCardContainer from "./GameCardContainer";
 import GameCard from "./GameCard";
+import React from "react";
 
 const GameGrid = () => {
+  const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames();
+
+  if (error) return <Text>{error.message}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
       spacing={6}
       padding={3}
     >
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
-      <GameCardContainer>
-        <GameCard />
-      </GameCardContainer>
+      {data?.pages.map((page, index) => (
+        <React.Fragment key={index}>
+          {page.results.map((game) => (
+            <GameCardContainer key={game.id}>
+              <GameCard game={game} />
+            </GameCardContainer>
+          ))}
+        </React.Fragment>
+      ))}
     </SimpleGrid>
   );
 };
