63fc39cade49519f827a44bb1efdd8ebd1bdbf3a
Elabscode <elabscodesocial@gmail.com>
2024-04-20 14:24:28 +0000

    Updated GenresList with API data

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,16 +1,45 @@
-import { Heading, List, ListItem } from "@chakra-ui/react";
+import {
+  HStack,
+  Heading,
+  List,
+  ListItem,
+  Spinner,
+  Image,
+  Button,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
 
 const GenreList = () => {
+  const { data, error, isLoading } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
     <>
       <Heading fontSize="2xl" marginBottom={3}>
         Genres
       </Heading>
       <List>
-        <ListItem>Genre 1</ListItem>
-        <ListItem>Genre 2</ListItem>
-        <ListItem>Genre 3</ListItem>
-        <ListItem>Genre 4</ListItem>
+        {data?.results.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={genre.image_background}
+                boxSize="32px"
+                borderRadius={8}
+                objectFit="cover"
+              />
+              <Button
+                variant="link"
+                whiteSpace="normal"
+                textAlign="left"
+                fontSize="large"
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
       </List>
     </>
   );
