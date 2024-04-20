46ea4cdc92457d1a5d7a223eb6f65981db1d153b
Elabscode <elabscodesocial@gmail.com>
2024-04-20 20:52:15 +0000

    Added GameCardSkeleton while loaading games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,13 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCardContainer from "./GameCardContainer";
+import GameCardSkeleton from "./GameCardSkeleton";
 import GameCard from "./GameCard";
 import React from "react";
 
 const GameGrid = () => {
   const { data, error, isLoading, fetchNextPage, hasNextPage } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
   if (error) return <Text>{error.message}</Text>;
   return (
@@ -14,6 +16,12 @@ const GameGrid = () => {
       spacing={6}
       padding={3}
     >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <GameCardContainer key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
       {data?.pages.map((page, index) => (
         <React.Fragment key={index}>
           {page.results.map((game) => (
