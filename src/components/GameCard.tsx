9f287d058029cfff8c8f0b5df9a15591b52abbb2
Elabscode <elabscodesocial@gmail.com>
2024-04-20 20:54:39 +0000

    Added Platform Icons and Ratings

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,9 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import Game from "../entities/Game";
 import getCroppedImageUrl from "../services/image-url";
 import { Link } from "react-router-dom";
+import PlatformIconList from "./PlatformIconList";
+import CriticScore from "./CriticScore";
 interface Props {
   game: Game;
 }
@@ -10,6 +12,12 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
         <Heading fontSize="2xl">
           <Link to={`/games/${game.slug}`}>{game.name}</Link>
         </Heading>
